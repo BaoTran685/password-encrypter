98bc4443ca87a275dc23898731816d1ee1988c30
Bao Tran <baopei05@gmail.com>
2023-08-24 10:08:38 -0300

    Fixed Css Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,22 +6,22 @@ const NavBar = () => {
   const {data: session}=useSession();
   return (
     <nav>
+      
       <div className="header">
-        <h1>Password Encrypter</h1>
+        <Link href='/' className="title"><h1>Password Encrypter</h1></Link>
       </div>
       <div className="wrap">
         {session?.user ? 
         (
-          <Link href='/update'><p>Update</p></Link>
+          <Link href='/update' className="a"><p>Update</p></Link>
         ) : 
         (
           <></>
         )
         }
-        <Link href='/'><p>Home</p></Link>
-        <Link href='/generate'><p>Generate</p></Link>
-        <Link href='/function'><p>Encrypt/Decrypt</p></Link>
-        <Link href='/about'><p>About</p></Link>
+        <Link href='/generate' className="a"><p>Generate</p></Link>
+        <Link href='/function' className="a"><p>Encrypt/Decrypt</p></Link>
+        <Link href='/about' className="a"><p>About</p></Link>
         {session?.user ? 
         (
           <button onClick={signOut}>Log Out</button>
