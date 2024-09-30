6b7f1ddc7623c490838f17814f041a5d2fb31d52
Bao Tran <baopei05@gmail.com>
2024-09-30 00:26:09 -0400

    new comments about the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Password Encrypter || Home",
   description: "Password Encryption Home Page",
 };
 
+const password_encrypter_link = "https://passwordencrypter.com/";
 const Home = () => {
   return (
     <main className="my-10 sm:my-20">
       <div className="flex px-6 sm:px-8 lg:px-10">
         <ol className="text--content list-disc space-y-2 sm:space-y-4">
-          <li>Hello...! This app is the renew version of the ...</li>
+          <li>
+            Hello, Welcome to Password Encrypter!!! This app was created in my
+            grade 12 summer. Well, to be honest this is the newer version and I
+            have attached the link to the older one{" "}
+            <Link
+              href={password_encrypter_link}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="underline decoration-2 decoration-blue-400"
+            >
+              here
+            </Link>
+            .
+          </li>
           <li>
             <span className="text-[--red-color] font-bold">
               For the Generate Page,
